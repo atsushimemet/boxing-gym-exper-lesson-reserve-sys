d41e81be4d8d61f096e0ffb825c52a660802ec0e
atsushimemet <noap3b69n@gmail.com>
2023-05-21 15:25:14 +0900

    Update #6 gym_phone_reserve_linkのテストの追加

--- reserve/tests.py
+++ reserve/tests.py
@@ -122,3 +122,23 @@ class GymModelTest(TestCase):
 
         gym_addr_field = Gym._meta.get_field("gym_addr")
         self.assertEqual(gym_addr_field.max_length, 100)
+
+    def test_gym_phone_reserve_link_field_type(self):
+        self.setup()
+        field = self.gym._meta.get_field("gym_phone_reserve_link")
+        self.assertEqual(field.get_internal_type(), "CharField")
+
+    def test_gym_phone_reserve_link_has_correct_max_length(self):
+        gym = Gym(
+            gym_cd=4,
+            gym_nm="Example Gym",
+            gym_hp_link="http://example.com",
+            gym_addr="Example Address",
+            gym_phone_reserve_link="A" * 256,
+            gym_net_reserve_link="http://example.com/net",
+        )
+        with self.assertRaises(Exception):
+            gym.full_clean()  # cleanメソッドを呼び出し、例外が発生することを確認します
+
+        gym_phone_reserve_link_field = Gym._meta.get_field("gym_phone_reserve_link")
+        self.assertEqual(gym_phone_reserve_link_field.max_length, 255)
