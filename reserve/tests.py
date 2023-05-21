0ee0a30ec9a3a7b1362c214d0b0af3d9fb310f09
atsushimemet <noap3b69n@gmail.com>
2023-05-21 15:12:58 +0900

    Update #6 gym_nmのテストの更新

--- reserve/tests.py
+++ reserve/tests.py
@@ -30,3 +30,27 @@ class GymModelTest(TestCase):
         self.setup()
         field = self.gym._meta.get_field("gym_cd")
         self.assertEqual(field.get_internal_type(), "IntegerField")
+
+    def test_gym_nm_is_unique(self):
+        # gym_nmが一意制約を持つことをテストします。
+        self.setup()
+        field = self.gym._meta.get_field("gym_nm")
+        self.assertTrue(field.unique)
+
+    def test_gym_nm_does_not_allow_null(self):
+        # nullを許容していないフィールドのテスト
+        gym = Gym(
+            gym_cd=2,
+            gym_nm=None,  # null値を指定します
+            gym_hp_link="http://example.com",
+            gym_addr="Example Address",
+            gym_phone_reserve_link="http://example.com/phone",
+            gym_net_reserve_link="http://example.com/net",
+        )
+        with self.assertRaises(Exception):
+            gym.full_clean()  # cleanメソッドを呼び出し、例外が発生することを確認します
+
+    def test_gym_nm_field_type(self):
+        self.setup()
+        field = self.gym._meta.get_field("gym_nm")
+        self.assertEqual(field.get_internal_type(), "CharField")
