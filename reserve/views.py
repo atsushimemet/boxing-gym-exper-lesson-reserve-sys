a6859b4eea4348eb6fc571cb8873338127be2c98
atsushimemet <noap3b69n@gmail.com>
2023-05-21 16:01:01 +0900

    Feature issue 8 (#9)
    * Update #6 viewsの更新
    
    * Update #6 htmlの更新

--- reserve/views.py
+++ reserve/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
+from .models import Gym
+
 
 # Create your views here.
 def index(request):
+    gyms = Gym.objects.all()
     return render(
         request,
         "reserve/index.html",
-        {},
+        {"gyms": gyms},
     )
