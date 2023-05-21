3777c640baa282a1fe2976bfe05a515d118875a6
atsushimemet <noap3b69n@gmail.com>
2023-05-21 15:37:30 +0900

    Refactor #6 GymModelTestクラスの分割

--- reserve/tests.py
+++ reserve/tests.py
@@ -15,29 +15,39 @@ class GymModelTest(TestCase):
             gym_net_reserve_link="http://example.com/net",
         )
 
-    def test_gym_cd_is_primary_key(self):
+
+class GymModelGymCdTest(GymModelTest):
+    def setup(self):
+        super().setup()
+
+    def test_is_primary_key(self):
         # gym_cdが主キーであることをテストします。
         self.setup()
         self.assertEqual(self.gym._meta.pk.name, "gym_cd")
 
-    def test_gym_cd_is_unique(self):
+    def test_is_unique(self):
         # gym_cdが一意制約を持つことをテストします。
         self.setup()
         field = self.gym._meta.get_field("gym_cd")
         self.assertTrue(field.unique)
 
-    def test_gym_cd_field_type(self):
+    def test_field_type(self):
         self.setup()
         field = self.gym._meta.get_field("gym_cd")
         self.assertEqual(field.get_internal_type(), "IntegerField")
 
-    def test_gym_nm_is_unique(self):
+
+class GymModelGymNmTest(GymModelTest):
+    def setup(self):
+        super().setup()
+
+    def test_is_unique(self):
         # gym_nmが一意制約を持つことをテストします。
         self.setup()
         field = self.gym._meta.get_field("gym_nm")
         self.assertTrue(field.unique)
 
-    def test_gym_nm_does_not_allow_null(self):
+    def test_does_not_allow_null(self):
         # nullを許容していないフィールドのテスト
         gym = Gym(
             gym_cd=2,
@@ -50,12 +60,12 @@ class GymModelTest(TestCase):
         with self.assertRaises(Exception):
             gym.full_clean()  # cleanメソッドを呼び出し、例外が発生することを確認します
 
-    def test_gym_nm_field_type(self):
+    def test_field_type(self):
         self.setup()
         field = self.gym._meta.get_field("gym_nm")
         self.assertEqual(field.get_internal_type(), "CharField")
 
-    def test_gym_nm_has_correct_max_length(self):
+    def test_has_correct_max_length(self):
         gym = Gym(
             gym_cd=3,
             gym_nm="A" * 101,  # 101文字の文字列を指定します
@@ -70,12 +80,17 @@ class GymModelTest(TestCase):
         gym_nm_field = Gym._meta.get_field("gym_nm")
         self.assertEqual(gym_nm_field.max_length, 100)
 
-    def test_gym_hp_link_field_type(self):
+
+class GymModelGymHpLinkTest(GymModelTest):
+    def setup(self):
+        super().setup()
+
+    def test_field_type(self):
         self.setup()
         field = self.gym._meta.get_field("gym_hp_link")
         self.assertEqual(field.get_internal_type(), "CharField")
 
-    def test_gym_hp_link_has_correct_max_length(self):
+    def test_has_correct_max_length(self):
         gym = Gym(
             gym_cd=4,
             gym_nm="Example Gym",
@@ -90,7 +105,12 @@ class GymModelTest(TestCase):
         gym_hp_link_field = Gym._meta.get_field("gym_hp_link")
         self.assertEqual(gym_hp_link_field.max_length, 255)
 
-    def test_gym_addr_does_not_allow_null(self):
+
+class GymModelGymAddrTest(GymModelTest):
+    def setup(self):
+        super().setup()
+
+    def test_does_not_allow_null(self):
         # nullを許容していないフィールドのテスト
         gym = Gym(
             gym_cd=2,
@@ -103,12 +123,12 @@ class GymModelTest(TestCase):
         with self.assertRaises(Exception):
             gym.full_clean()  # cleanメソッドを呼び出し、例外が発生することを確認します
 
-    def test_gym_addr_field_type(self):
+    def test_field_type(self):
         self.setup()
         field = self.gym._meta.get_field("gym_addr")
         self.assertEqual(field.get_internal_type(), "CharField")
 
-    def test_gym_addr_has_correct_max_length(self):
+    def test_has_correct_max_length(self):
         gym = Gym(
             gym_cd=3,
             gym_nm="A" * 101,  # 101文字の文字列を指定します
@@ -123,12 +143,17 @@ class GymModelTest(TestCase):
         gym_addr_field = Gym._meta.get_field("gym_addr")
         self.assertEqual(gym_addr_field.max_length, 100)
 
-    def test_gym_phone_reserve_link_field_type(self):
+
+class GymModelGymPhoneReserveLinkTest(GymModelTest):
+    def setup(self):
+        super().setup()
+
+    def test_field_type(self):
         self.setup()
         field = self.gym._meta.get_field("gym_phone_reserve_link")
         self.assertEqual(field.get_internal_type(), "CharField")
 
-    def test_gym_phone_reserve_link_has_correct_max_length(self):
+    def test_has_correct_max_length(self):
         gym = Gym(
             gym_cd=4,
             gym_nm="Example Gym",
@@ -143,12 +168,17 @@ class GymModelTest(TestCase):
         gym_phone_reserve_link_field = Gym._meta.get_field("gym_phone_reserve_link")
         self.assertEqual(gym_phone_reserve_link_field.max_length, 255)
 
-    def test_gym_net_reserve_link_field_type(self):
+
+class GymModelGymNetReserveLinkTest(GymModelTest):
+    def setup(self):
+        super().setup()
+
+    def test_field_type(self):
         self.setup()
         field = self.gym._meta.get_field("gym_net_reserve_link")
         self.assertEqual(field.get_internal_type(), "CharField")
 
-    def test_gym_net_reserve_link_has_correct_max_length(self):
+    def test_has_correct_max_length(self):
         gym = Gym(
             gym_cd=4,
             gym_nm="Example Gym",
