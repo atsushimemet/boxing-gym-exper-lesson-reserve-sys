f39239febbbb96bbafc3b886001e8b66f003c777
atsushimemet <noap3b69n@gmail.com>
2023-05-21 14:36:11 +0900

    Add #6 GymModelTestの追加

--- reserve/tests.py
+++ reserve/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Gym
+
+
+class GymModelTest(TestCase):
+    def setup(self):
+        # テストに必要な初期データをセットアップする場合は、setUpメソッドを使用します。
+        self.gym = Gym.objects.create(
+            gym_cd=1,
+            gym_nm="Example Gym",
+            gym_hp_link="http://example.com",
+            gym_addr="Example Address",
+            gym_phone_reserve_link="http://example.com/phone",
+            gym_net_reserve_link="http://example.com/net",
+        )
