a2a2423baa407a6318d16aafed55128a9ba3a6e6
atsushimemet <noap3b69n@gmail.com>
2023-05-21 14:32:21 +0900

    Add #6 Gymモデルの追加

--- reserve/models.py
+++ reserve/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Gym(models.Model):
+    gym_cd = models.IntegerField(primary_key=True, unique=True)
+    gym_nm = models.CharField(unique=True, null=False, max_length=100)
+    gym_hp_link = models.CharField(unique=True, max_length=255)
+    gym_addr = models.CharField(null=False, max_length=100)
+    gym_phone_reserve_link = models.CharField(max_length=255)
+    gym_net_reserve_link = models.CharField(max_length=255)
